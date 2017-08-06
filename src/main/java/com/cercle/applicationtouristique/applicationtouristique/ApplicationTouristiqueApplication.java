07dc0ead3e8ed0687fe041a794e0ddd3f5ea44da
Christopher LE BLOAS <christopher.lebloas@gmail.com>
2017-08-06 23:06:37 +0200

    init project

---
+++ src/main/java/com/cercle/applicationtouristique/applicationtouristique/ApplicationTouristiqueApplication.java
@@ -0,0 +1,12 @@
+package com.cercle.applicationtouristique.applicationtouristique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTouristiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTouristiqueApplication.class, args);
+	}
+}
